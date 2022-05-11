eeada01cbf7f660efc4f049a40a30fcdeea30b4e
Youngchae James Chee <litcoderr@gmail.com>
2022-05-11 22:44:17 +0900

    main: implemented optimizer

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import os
 from tqdm import tqdm
 from importify import Serializable
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 
@@ -29,16 +30,18 @@ def collate_fn(b):
     batch_tensor = torch.cat(padded_list, 0) 
 
     batch_label = torch.cat(label_list, 0)
+    batch_label = torch.squeeze(batch_label, 1)
+    batch_label = batch_label.long()
     return batch_tensor, batch_label
 
 def validate_checkpoint(path):
-    start_batch_idx = 0
+    start_epoch_idx = 0
     if os.path.exists(path):
         # TODO check if valid
-        start_batch_idx = int(path.split("/")[-1].split(".ckpt")[0])
-        return start_batch_idx, path
+        start_epoch_idx = int(path.split("/")[-1].split(".ckpt")[0])
+        return start_epoch_idx, path
     else:
-        return start_batch_idx, None
+        return start_epoch_idx, None
 
 class TrainConfig(Serializable):
     def __init__(self):
@@ -46,9 +49,12 @@ class TrainConfig(Serializable):
         self.config_file = None
         self.device = "cuda"
         # dataloader options
-        self.batch_size = 16
+        self.batch_size = 32
         self.shuffle = True
-        self.num_workers = 8
+        self.num_workers = 16
+        
+        # learning rate
+        self.lr = 0.001
 
         # checkpoint option
         # where the training should start from
@@ -73,27 +79,38 @@ if __name__ == "__main__":
                             collate_fn=collate_fn)
 
     # TODO implement checkpointing process
-    start_batch_idx, ckpt_path = validate_checkpoint(path=os.path.join(current_dir,
+    start_epoch_idx, ckpt_path = validate_checkpoint(path=os.path.join(current_dir,
                                                   "SentimentAnalyzer",
                                                   "checkpoints",
                                                   "{}.ckpt".format(config.checkpoint)))
     
     app = App(model_config=config.model_config, checkpoint=ckpt_path, device=config.device)
 
-    batch_idx = start_batch_idx
+    # criterion
+    criterion = nn.CrossEntropyLoss()
+
+    # optimizer
+    optimizer = torch.optim.Adam(app.model.parameters(), lr=config.lr)
+
+    epoch_idx = start_epoch_idx
     while True:
         pbar = tqdm(train_dataloader)
         for (tensor, label) in pbar:
-            pbar.set_description("[batch #{}]".format(batch_idx))
+            pbar.set_description("[epoch #{}]".format(epoch_idx))
 
+            # output: [b, n_classes]
+            # label: [b]
             output = app.forward(tensor, is_train=True)
-            print(output.shape)
 
-            # TODO compute gradient
-            # TODO back propagate
-            break
-        break
+            # compute loss
+            label = label.to(config.device)
+            loss = criterion(input=output, target=label)
+
+            # back propagate
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
             
-        batch_idx += 1
+        epoch_idx += 1
 
         # TODO save checkpoint / config
