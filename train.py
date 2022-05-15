main: fixed average loss calcuation:

train.py
@@ -131,7 +131,7 @@ if __name__ == "__main__":
             loss.backward()
             optimizer.step()
         total_acc = total_acc.cpu().numpy()
-        total_loss /= len(train_dataloader) 
+        total_loss /= len(train_dataset) 
         total_acc = (total_acc / len(train_dataset)) * 100
 
         writer.add_scalar("Loss/train", total_loss, epoch_idx)
@@ -164,7 +164,7 @@ if __name__ == "__main__":
                     true_pos = torch.sum(compared)
                     total_acc += true_pos
                 total_acc = total_acc.cpu().numpy()
-                total_loss /= len(test_dataloader) 
+                total_loss /= len(test_dataset) 
                 total_acc = (total_acc / len(test_dataset)) * 100
 
                 writer.add_scalar("Loss/test", total_loss, epoch_idx)